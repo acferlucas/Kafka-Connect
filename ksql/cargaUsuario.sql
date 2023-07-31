6861bddbb6784dd1df9bf69c5074d641200ac090
acferlucas <acferlucas.dev@gmail.com>
2023-07-31 15:04:22 -0300

    add sink

--- ksql/cargaUsuario.sql
+++ ksql/cargaUsuario.sql
@@ -11,9 +11,15 @@ SELECT
 FROM
   USUARIO_STREAM_T1 EMIT CHANGES;
 
-CREATE STREAM SINK_USUARIO AS
-  SELECT
-    NOME_DE_USUARIO AS USER_NAME,
-    SENHA           AS USER_PASS
-  FROM
-    USUARIO_STREAM;
+CREATE STREAM USUARIO_SINK_STREAM AS
+SELECT
+  AFTER->NOME AS USER_NAME,
+  AFTER->SENHA AS USER_PASS
+FROM USUARIO_STREAM;
+
+CREATE SINK CONNECTOR USUARIO_SINK WITH (
+  'connector.class' = 'org.apache.kafka.connect.file.FileStreamSinkConnector',
+  'file' = '/tmp/usuario_sink_stream.json',
+  'format' = 'json',
+  'topics' = 'USUARIO_SINK_STREAM'
+);
